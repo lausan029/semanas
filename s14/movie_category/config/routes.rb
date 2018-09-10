3c1ce1d4a06bed7177253ba0e6e47cddaba7896b
lsanchez <lau.san029@gmail.com>
2018-09-10 18:20:19 -0300

    s14

--- s14/movie_category/config/routes.rb
+++ s14/movie_category/config/routes.rb
@@ -1,11 +1,5 @@
 Rails.application.routes.draw do
-  get 'pages/envio'
-  get 'pages/recibo'
-  post 'pages/recibo'
-
-  get 'pages/my'
-  get 'pages/generator'
-  get 'pages/save_page'
-
+  resources :movies
+  resources :categories
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
